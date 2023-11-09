30990158f5cb04ae5d2646050c8f166319290f0b
Trunkedd <Jahn12jensen@gmail.com>
2023-11-09 17:14:23 +0100

    yadadadbooblaa
    mhmhmmh

---
+++ game.py
@@ -0,0 +1,52 @@
+import pygame as pg
+from sprites import *
+import random
+pg.init() # starter pygame modul
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+YELLOW = (255,255,0)
+
+screen = pg.display.set_mode((800,600)) # lager spill vindu, 800x600
+clock = pg.time.Clock()
+font_times40 = pg.font.SysFont("Times New Roman",20)
+
+all_sprites = pg.sprite.Group()
+enemies = pg.sprite.Group()
+
+
+player = Player(all_sprites,enemies) # lager 1 kopi av Player class
+all_sprites.add(player) # legg til player i gruppen
+
+
+playing = True
+while playing: # game loop
+    clock.tick(120)
+    #print("FPS: ", i)
+    for event in pg.event.get():
+        if event.type == pg.QUIT: # hvis vi trykker pÃ¥ krysset i spillvinduet
+            playing = False
+            pg.quit()
+
+    # spawning av flere players
+    if len(enemies) < 10:
+        new_enemy = Enemy()
+        all_sprites.add(new_enemy)
+        enemies.add(new_enemy)
+
+    # oppdater alle sprites i all_sprites gruppen
+    all_sprites.update()
+
+    hits = pg.sprite.spritecollide(player, enemies, True)
+    if hits:
+        player.take_dmg(10)
+        print(player.hp)
+
+    hp_text = font_times40.render(f"HP: {player.hp}", False ,(GREEN))
+    # tegn bakgrunn og alle sprites
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    screen.blit(hp_text,(10,10))
+    pg.display.update()
---
+++ sprites.py
@@ -0,0 +1,136 @@
+import pygame as pg
+import random
+
+running = pg.image.load('images/playerrun1.png')
+running2 = pg.image.load('images/playerrun2.png')
+running3 = pg.image.load('images/playerrun3.png')
+running4 = pg.image.load('images/playerrun4.png')
+running5 = pg.image.load('images/playerrun5.png')
+running6 = pg.image.load('images/playerrun6.png')
+running7 = pg.image.load('images/playerrun7.png')
+running8 = pg.image.load('images/playerrun8.png')
+
+running = pg.transform.scale(running,(35,35))
+running2 = pg.transform.scale(running2,(35,35))
+running3 = pg.transform.scale(running3,(35,35))
+running4 = pg.transform.scale(running4,(35,35))
+running5 = pg.transform.scale(running5,(35,35))
+running6 = pg.transform.scale(running6,(35,35))
+running7 = pg.transform.scale(running7,(35,35))
+running8 = pg.transform.scale(running8,(35,35))
+player_image = pg.image.load("images/player.png")
+player1=pg.image.load("images/player1.png")
+player1=pg.transform.scale(player1,(35,35))
+player_imageimage = pg.transform.scale(player_image,(35,35))
+enemy_image = pg.image.load("images/enemy.png")
+ranged_image = pg.image.load("images/lasanga.png")
+ranged_imageimage = pg.transform.scale(ranged_image,(30,30))
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, all_sprites,enemies): # denne funksjonen kjører når vi lager player
+        pg.sprite.Sprite.__init__(self)
+        self.current_frame = 0
+        self.last_update = 0
+        self.standing = True
+        self.running = False
+        self.image = player_imageimage
+        self.rect = self.image.get_rect()
+        self.standing_frames =[player1,player_imageimage]
+        self.running_frames = [running,running2,running3,running4,running5,running6,running7,running8]
+        self.pos_x = 50
+        self.pos_y = 400
+        self.speed = 3
+        self.hp = 100
+        self.all_sprites = all_sprites
+        self.enemies = enemies
+
+    def take_dmg(self, dmg):
+        self.hp -= dmg 
+        if self.hp <= 0:
+            self.kill()
+
+    def attack(self):
+        projectile = ranged_attack(self.pos_x, self.pos_y,self.enemies)
+        print("attacked")
+        projectile.add(self.all_sprites)
+        
+
+    def update(self):
+        self.animate()
+        self.rect.centerx = self.pos_x
+        self.rect.centery = self.pos_y
+        self.standing = True
+        
+        if self.pos_x > 900:
+            self.pos_x = 900
+        
+       
+
+        # player input
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w]: # oppover
+            self.pos_y -= self.speed
+            self.standing=False
+            self.running=True
+        if keys[pg.K_s]: # nedover
+            self.pos_y += self.speed
+            self.standing=False
+            self.running=True
+        if keys[pg.K_a]: # venstre
+            self.pos_x -= self.speed
+            self.standing=False
+            self.running=True
+        if keys[pg.K_d]: # høyre
+            self.pos_x += self.speed 
+            self.standing=False
+            self.running=True
+        if keys[pg.K_f]:
+            self.attack()
+    def animate(self):
+        now = pg.time.get_ticks()
+        if self.standing:
+            if now - self.last_update > 350:
+                self.last_update = now
+                self.current_frame =(self.current_frame +1)%len(self.standing_frames)
+                self.image = self.standing_frames[self.current_frame]
+                self.rect=self.image.get_rect()
+        if self.running:
+            if now - self.last_update > 350:
+                self.last_update = now
+                self.current_frame = (self.current_frame +1)%len(self.running_frames)
+                self.image = self.running_frames[self.current_frame]
+                self.rect=self.image.get_rect()
+
+class Enemy(pg.sprite.Sprite):
+    def __init__(self): # denne funksjonen kjører når vi lager player
+        pg.sprite.Sprite.__init__(self)
+        self.image = enemy_image
+        self.rect = self.image.get_rect()
+        self.pos_x = 900
+        self.pos_y = random.randint(0,600)
+        self.speed = random.randint(1,5)
+
+    def update(self):
+        self.rect.centerx = self.pos_x
+        self.rect.centery = self.pos_y
+        self.pos_x -= self.speed
+        if self.pos_x < -10:
+            self.kill()
+class ranged_attack(pg.sprite.Sprite):
+    def __init__(self,x,y,enemies):
+        pg.sprite.Sprite.__init__(self)
+        self.image = ranged_imageimage
+        self.rect = self.image.get_rect()
+
+        self.pos_x = x
+        self.pos_y = y
+        self.speed = 2
+        self.enemies = enemies
+
+    def update(self):
+        self.rect.x = self.pos_x
+        self.rect.y = self.pos_y
+        self.pos_x += self.speed
+        hits = pg.sprite.spritecollide(self,self.enemies, True)
+
+
